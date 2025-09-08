738e299320c84bc47eb188213f7800c5a5c7c94a
John <axercute@gmail.com>
2025-09-08 10:30:11 +0800

    trying to deploy #8

--- src/routes/api/stripe/createCustomer/+server.js
+++ src/routes/api/stripe/createCustomer/+server.js
@@ -1,36 +1,37 @@
-import Stripe from 'stripe';
-import { env } from '$env/dynamic/private';
-const stripe = new Stripe(env.STRIPEKEY);
-export const POST=async()=>{
-try{
-const customer = await stripe.customers.create({
-  email: 'customer123@example.com',
-  name:"Customer 959"
-});
-console.log(customer)
-return new Response(customer.id);
-}
-catch(error){
-  const errorMessage = "unable to create customer"
-  console.log(errorMessage,error)
-  return new Response(errorMessage,error,{status:500})
-}
-}
-
-
-// export async function GET() {
-// const text = "testing"
-
-//   return new Response("test"
-//   );
+// import Stripe from 'stripe';
+// import { env } from '$env/dynamic/private';
+// import { json } from '@sveltejs/kit';
+// const stripe = new Stripe(env.STRIPEKEY,{apiVersion: '2022-11-15'});
+// export const POST=async()=>{
+// try{
+// const customer = await stripe.customers.create({
+//   email: 'customer123@example.com',
+//   name:"Customer 959"
+// });
+// console.log(customer)
+// return json(customer.id);
+// }
+// catch(error){
+//   const errorMessage = "unable to create customer"
+//   console.log(errorMessage,error)
+//   return json(errorMessage,error,{status:500})
+// }
 // }
 
 
+// // export async function GET() {
+// // const text = "testing"
 
-// export async function GET() {
-//   return new Response("Hello from API!", {
-//     headers: {
-//       "Content-Type": "text/plain"
-//     }
-//   });
-// }
+// //   return new Response("test"
+// //   );
+// // }
+
+
+
+// // export async function GET() {
+// //   return new Response("Hello from API!", {
+// //     headers: {
+// //       "Content-Type": "text/plain"
+// //     }
+// //   });
+// // }
