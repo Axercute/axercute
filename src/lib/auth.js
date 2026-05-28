2865e5ed604d90690fd20f1637b97d66db88bd9b
John <axercute@gmail.com>
2026-05-28 16:18:53 +0800

    trying better-auth

---
+++ src/lib/auth.js
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { env } from '$env/dynamic/private';
+const client = new MongoClient(env.DATABASE_URL);
+const db = client.db();
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    // Optional: if you don't provide a client, database transactions won't be enabled.
+    client
+  }),
+  emailAndPassword: { 
+    enabled: true, 
+  }, 
+
+});
